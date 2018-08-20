25dd439f4f2a84c037b6e52977d38add91e0e5f2
Emi Ko <emi8emi@gmail.com>
2018-08-20 10:15:45 -0700

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget(p) {
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+  const x = document.querySelectorAll('.ranked-list')
+
+for (let i = 0, l = x.length; i < l; i++) {
+  let children = x[i].children
+
+  for (let j = 0, k = children.length; j < k; j++) {
+    children[j].innerHTML = parseInt(children[j].innerHTML) + n
+  }
+}
+}
+
+function deepestChild() {
+  let child = document.getElementById('grand-node').querySelectorAll('div')
+  return child[child.length - 1]
+}
